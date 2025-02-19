42ef4a44533e4628f85331fa4917f978e00bb209
dhdschool <dhdschool@outlook.com>
2025-02-18 20:11:45 -0600

    Add multiclass prediction model

--- src/gnn.py
+++ src/gnn.py
@@ -6,6 +6,11 @@ import pandas as pd
 from pyvis.network import Network
 from time import time
 import random
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import OneHotEncoder
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 def gpu_check():
     print(f"GPU is avaliable: {torch.cuda.is_available()}")
@@ -53,8 +58,6 @@ def render_graph_sample(graph, target, k=2):
     target = [target[i] for i in sample.nodes]
     render_graph(sample, target)
     
-    
-    
 def format_kc(graph: nx.Graph):
     map = {}
     
@@ -71,10 +74,6 @@ def format_kc(graph: nx.Graph):
     mapped_graph.add_nodes_from(nodes)
     mapped_graph.add_edges_from(edges)
     return mapped_graph, map
-
-   
-    
-    
 
 class KSampler:
     def __init__(self, graph: nx.Graph, target, k=2):
@@ -138,8 +137,69 @@ class KSampler:
             
         return sample_graph, sample_target
 
+def graph_split(graph : nx.Graph, target, mapper: dict, feature_size: int=16):
+    test_nodes = np.array(list(mapper.keys())).astype(np.int32)
+    train_nodes = np.array(list(set(graph.nodes()).difference(set(mapper.keys())))).astype(np.int32)
+    
+    label_count = len(pd.unique(target))
+    y = np.zeros(shape=(target.shape[0], label_count))
+    for index, label in enumerate(target):
+        y[index, label] = 1
+    
+    embedder = Diff2Vec(diffusion_number=2, diffusion_cover=20, dimensions=feature_size)
+    embedder.fit(graph)
+    
+    X = np.array(embedder.get_embedding())
+    
+    X_train, X_test = X[train_nodes], X[test_nodes]
+    y_train, y_test = y[train_nodes, :], y[test_nodes, :]
+    
+    return X_train, y_train, X_test, y_test    
+
+def train(model: nn.Module, dataloader: DataLoader):
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters())
+
+    while True:
+        for X, y in dataloader:
+            optimizer.zero_grad()
+            
+            pred = model(X)
+            loss = loss_fn(pred, y)
+            
+            loss.backward()
+            optimizer.step()
+        yield loss
+        
+def validate(model: nn.Module, dataloader: DataLoader):
+    n = len(dataloader)
+    f1_sum  = 0
+    for X, y_true in dataloader:
+        y_pred = model(X)
+        f1_sum += f1_score(y_true, y_pred, average='binary')
+    return f1_sum / n
+    
+class EmbeddedDataset(Dataset):
+    def __init__(self, X, y):        
+        self.features = X
+        self.labels = y
+        
+        self.features = torch.Tensor(self.features).double()
+        self.labels = torch.Tensor(self.labels).int()
+        
+       
+    def __getitem__(self, key):
+        return self.features[key], self.labels[key]
+        
+    def __len__(self):
+        return len(self.features)
+
+
 if __name__ == '__main__':
     gpu_check()
+    
+    embedding_features = 16
+    epochs = 1
     
     reader = GraphReader('facebook')
     graph = reader.get_graph()
@@ -148,20 +208,37 @@ if __name__ == '__main__':
     sampler = KSampler(graph, target, k=2)
     sample_graph, sample_target, sample_mapper = sampler.sample_as_graph(kc=True)
     
-    test_nodes = np.array(list(sample_mapper.keys())).astype(np.int32)
-    train_nodes = np.array(list(set(graph.nodes()).difference(set(sample_mapper.keys())))).astype(np.int32)
-    
     print()
-    print(sample_graph)
+    print(f"Sample graph is a {sample_graph}")
+    print()
     
-    model = Diff2Vec(diffusion_number=2, diffusion_cover=20, dimensions=16)
-    model.fit(graph)
+    X_train, y_train, X_test, y_test = graph_split(graph, target, sample_mapper, feature_size=16)
     
-    X = np.array(model.get_embedding())
-    y = np.array(target)
-        
-    X_train, X_test = X[train_nodes], X[test_nodes]
-    y_train, y_test = y[train_nodes], y[test_nodes]
+    train_dataset = EmbeddedDataset(X_train, y_train)
+    test_dataset = EmbeddedDataset(X_test, y_test)
+    
+    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=8, shuffle=True)
+    
+    print("Dataset done loading!\n")
 
+    model = nn.Sequential(
+        nn.Linear(embedding_features, 32),
+        nn.Softmax(),
+        nn.Linear(32, 4),
+        nn.Softmax()
+    ) 
     
-    #render_graph(sample_graph, sample_target)
+    print("Training model...")
+    for epoch in range(epochs):
+        print(f'Epoch {epoch + 1}/{epochs}')
+        last_loss = train(model, train_dataloader)
+        print(f"Loss at epoch {epoch+1}: {last_loss}")
+    print()
+    
+    print("Evaluating model accuracy on sample...")
+    f1 = validate(model, test_dataloader)
+    print(f"Model has an f1 score of: {f1}\n")
+    
+    print("Rendering sample...")
+    render_graph(sample_graph, sample_target)
